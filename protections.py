26f3defca6a25f1fad9869c002996f21ffc0bf00
Luisqbd <83882610+Luisqbd@users.noreply.github.com>
2025-10-04 07:20:44 -0300

    feat: adiciona módulo de proteções contra scam

---
+++ protections.py
@@ -0,0 +1,8 @@
+class Protections:
+    def __init__(self, config):
+        self.config = config
+
+    def safe(self):
+        print("Checando proteções contra scam...")
+        # Lógica de proteção contra rug/honeypot
+        return True
